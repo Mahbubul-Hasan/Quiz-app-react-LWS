c623858f927741af50d7b228f6f55cb670824118
Mahbubul Hasan <mahbubul161@gmail.com>
2021-11-01 22:39:24 +0600

    Tooltip dynamically done

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "../asset/styles/ProgressBar.module.css";
 import Button from "./Button";
 
 export default function ProgressBar({ next, prev, submit, percentage }) {
+    const [tooltip, setTooltip] = useState(false);
+    const tooltipRef = useRef();
+
+    function toggleTooltip() {
+        if (tooltip) {
+            setTooltip(false);
+            tooltipRef.current.style.display = "none";
+        } else {
+            setTooltip(true);
+            tooltipRef.current.style.display = "block";
+            tooltipRef.current.style.left = `calc(${percentage}% - 65px)`;
+        }
+    }
+
     return (
         <div className={classes.progressBar}>
             <div className={classes.backButton} onClick={prev}>
                 <span className="material-icons-outlined"> arrow_back </span>
             </div>
             <div className={classes.rangeArea}>
-                <div className={classes.tooltip}>{percentage}% Complete!</div>
+                <div className={classes.tooltip} ref={tooltipRef}>
+                    {percentage}% Complete!
+                </div>
                 <div className={classes.rangeBody}>
-                    <div className={classes.progress} style={{ width: `${percentage}%` }} />
+                    <div
+                        onMouseOver={toggleTooltip}
+                        onMouseOut={toggleTooltip}
+                        className={classes.progress}
+                        style={{ width: `${percentage}%` }}
+                    />
                 </div>
             </div>
 
